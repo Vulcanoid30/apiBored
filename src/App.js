156cb493ade3eb9bc078c53272da5e8882011ce3
zLhYd <adigans221@gmail.com>
2022-10-13 22:51:38 +0700

    melakukan perubahan di file README.md

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import React, { useEffect, useState } from "react";
+// // import Test from "./components/test";
+// import Axios from "axios";
+import Test from "./components/test";
 
-function App() {
+const App = () => {
+  // const [number, setNumber] = useState(0);
+
+  // const rubah = (hitung) => {
+  //   if (hitung === "-") {
+  //     setNumber((state) => state - 1);
+  //   } else if (hitung === "+") {
+  //     setNumber((state) => state + 1);
+  //   }
+  // };
+  // const [name, setName] = useState("adi");
+
+  // const changeName = () => {
+  //   if (name === "adi") {
+  //     setName("zulhayad");
+  //   } else {
+  //     setName("adi");
+  //   }
+  // };
+  // const [datas, setDatas] = useState({});
+  // useEffect(() => {
+  //   Axios({
+  //     method: "GET",
+  //     url: `${process.env.REACT_APP_BASEURL}`,
+  //   }).then((result) => setDatas(result.data));
+  // }, []);
+  // console.log(datas);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Test />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/test.js
@@ -0,0 +1,44 @@
+import React, { createFactory } from "react";
+import "./test.css";
+import { useState, useEffect } from "react";
+import Select from "react-select";
+
+const Test = () => {
+  const [datas, getDatas] = useState([{}]);
+  const getBerry = async () => {
+    const api = await fetch("http://www.boredapi.com/api/activity/");
+    const getApi = await api.json();
+    const set = new Set([getApi]);
+    const result = Array.from(set).map((data) => {
+      return {
+        // accessibility: data.accessibility,
+        activity: data.activity,
+        key: data.key,
+        // link: data.link,
+        // participants: data.participants,
+        // price: data.price,
+        // type: data.type,
+      };
+    });
+    // console.log(result);
+    getDatas(result);
+  };
+  useEffect(() => {
+    getBerry();
+  }, []);
+
+  return (
+    <div>
+      <div className="button_container">
+        <button className="btn" onClick={getBerry}>
+          <span>Todo List</span>
+        </button>
+        <p className="description" key={datas[0]}>
+          {datas[0].activity}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Test;
